3aae82bb085faca337c32c2746aa866d42d4a06b
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-06-10 12:47:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const extendHex = (shortHex) => {
   // write your code here
-};
+	const extendHex = (shortHex) => {
+		
+  "#" +
+  shortHex
+    .slice(shortHex.startsWith("#") ? 1 : 0)
+    .split("")
+    .map((x) => x + x)
+    .join("");
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
+	}
+
+
+// Do not change the code below.
